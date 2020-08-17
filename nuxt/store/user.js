aa5017dccbbdce884984ba6a43405ba9b64d97bc
sonarAIT <sonarmikumiku@gmail.com>
2020-08-17 17:17:13 +0900

    Storeからemailを削除した

--- nuxt/store/user.js
+++ nuxt/store/user.js
@@ -1,16 +1,11 @@
 export const state = () => ({
     token: "aaa",
-    email: "aaa@aaa.com",
     name: "uouo"
 })
 
 export const mutations = {
     setToken(state,token){
         state.token = token;
-    },
-
-    setEmail(state,email){
-        state.email = email;
     },
 
     setName(state,name){
