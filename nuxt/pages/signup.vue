2b53f44679606589396d4f6c231e459db527fb47
kenshin1025 <sukoyakamind1025@gmail.com>
2020-08-17 16:48:33 +0900

    レスポンシブ対応してみた

--- nuxt/pages/signup.vue
+++ nuxt/pages/signup.vue
@@ -1,40 +1,42 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex 12>
-      <v-form ref="form" v-model="valid">
-        <h1>サインアップ</h1>
-        <v-text-field
-          v-model="name"
-          label="ユーザーネーム"
-          required
-          outlined
-        ></v-text-field>
+  <v-container>
+    <v-row justify="center">
+      <v-col md="4" sm="8" xs="10">
+          <v-form ref="form" v-model="valid" >
+            <h1>サインアップ</h1>
+            <v-text-field
+              v-model="name"
+              label="ユーザーネーム"
+              required
+              outlined
+            ></v-text-field>
 
-        <v-text-field
-          v-model="email"
-          label="E-mail"
-          required
-          outlined
-        ></v-text-field>
+            <v-text-field
+              v-model="email"
+              label="E-mail"
+              required
+              outlined
+            ></v-text-field>
 
-        <v-text-field
-          v-model="pass"
-          label="パスワード"
-          required
-          outlined
-        ></v-text-field>
+            <v-text-field
+              v-model="pass"
+              label="パスワード"
+              required
+              outlined
+            ></v-text-field>
 
-        <v-btn
-          :disabled="!valid"
-          color="success"
-          class="mr-4"
-          @click="create_user"
-        >
-          Validate
-        </v-btn>
-      </v-form>
-    </v-flex>
-  </v-layout>
+            <v-btn
+              :disabled="!valid"
+              color="success"
+              class="mr-4"
+              @click="create_user"
+            >
+              Validate
+            </v-btn>
+          </v-form>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
