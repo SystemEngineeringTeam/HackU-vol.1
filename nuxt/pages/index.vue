b45d12a1fefafdc69689012fb649fba7d359e2fb
sonarAIT <sonarmikumiku@gmail.com>
2020-08-17 15:47:15 +0900

    仮のタスクビューを作成した

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,97 +1,40 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
+  <v-row class="text-center">
+    <v-col cols="12" v-for="(task,i) in tasks" :key="i">
       <div class="text-center">
-        <logo />
-        <vuetify-logo />
+        <Task :task="task" />
       </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Task from "../components/Task";
 
 export default {
   components: {
-    Logo,
-    VuetifyLogo
-  }
+    Task
+  },
+
+  data: () => ({
+    tasks: [
+      {
+        id: 0,
+        name: "ごはん　たべる",
+        deadlineDate: "2020-08-21",
+        deadlineTime: "12:00:00",
+        description: "0",
+        weight: "0",
+      },
+      {
+        id: 1,
+        name: "うああああ",
+        deadlineDate: "2020-8-22",
+        deadlineTime: "09:12:00",
+        description: "0",
+        weight: "1"
+      }
+    ]
+  }),
 }
 </script>
