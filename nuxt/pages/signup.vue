07978110a111505688b9013c617393ca7c97493a
kenshin1025 <sukoyakamind1025@gmail.com>
2020-08-17 15:25:01 +0900

    ユーザー作成フォームの見た目を作った

--- nuxt/pages/signup.vue
+++ nuxt/pages/signup.vue
@@ -1,11 +1,58 @@
 <template>
-  <v-layout>
-    サインアップ
+  <v-layout column justify-center align-center>
+    <v-flex 12>
+      <v-form ref="form" v-model="valid">
+        <h1>サインアップ</h1>
+        <v-text-field
+          v-model="name"
+          label="ユーザーネーム"
+          required
+          outlined
+        ></v-text-field>
+
+        <v-text-field
+          v-model="email"
+          label="E-mail"
+          required
+          outlined
+        ></v-text-field>
+
+        <v-text-field
+          v-model="pass"
+          label="パスワード"
+          required
+          outlined
+        ></v-text-field>
+
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="create_user"
+        >
+          Validate
+        </v-btn>
+      </v-form>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      valid: false,
+      name: '',
+      email: '',
+      pass: '',
+    }
+  },
+  methods: {
+    create_user() {
+      console.log(this.name, this.email, this.pass)
+    },
+  },
+}
 </script>
 
 <style></style>
