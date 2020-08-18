c1c15738fc12f0efaa338f22b1454a321968101b
tikuwa <haru11502@ezweb.ne.jp>
2020-08-18 13:25:14 +0900

    タスク一覧取得の関数を追加しました

--- go/app/dbctl/user.go
+++ go/app/dbctl/user.go
@@ -1,1 +1,25 @@
 package dbctl
+
+// User はデータベースから取得したユーザーのデータを扱うための構造体
+type User struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Pass  string `json:"pass"`
+	Token string `json:"token"`
+}
+
+func callUserIDFromToken(token string) (int, error) {
+	rows, err := db.Query("select id from users where token=?", token)
+	if err != nil {
+		return -1, err
+	}
+	defer rows.Close()
+
+	userID := -1
+	for rows.Next() {
+		rows.Scan(&userID)
+	}
+
+	return userID, nil
+}
