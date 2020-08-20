00c5c02e9f83f42fc1d7c2350a2b807494f1d6c0
sonarAIT <sonarmikumiku@gmail.com>
2020-08-20 13:42:33 +0900

    HPバーとログを入れる部分を作成

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <v-row class="text-center">
+    <v-col cols="12">
+      <Logbox />
+    </v-col>
     <v-col cols="12" v-for="(task, i) in $store.state.tasks.tasks" :key="i">
       <div class="text-center">
         <Task :task="task" />
@@ -12,11 +15,13 @@
 <script>
 import Task from '../components/Task'
 import postDialog from '../components/postDialog'
+import Logbox from '../components/Logbox'
 
 export default {
   components: {
     Task,
     postDialog,
+    Logbox,
   },
 
   data: () => ({}),
