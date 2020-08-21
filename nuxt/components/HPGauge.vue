82326453146d92cea64a83cc0c7d9d2434a812b7
sonarAIT <sonarmikumiku@gmail.com>
2020-08-21 14:11:29 +0900

    HPゲージをComponentに分割

---
+++ nuxt/components/HPGauge.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-progress-linear
+      v-if="getHP <= 25"
+      height="25"
+      color="red"
+      :value="getHP"
+    ></v-progress-linear>
+    <v-progress-linear
+      v-else-if="getHP <= 50"
+      height="25"
+      color="yellow"
+      :value="getHP"
+    ></v-progress-linear>
+    <v-progress-linear
+      v-else
+      height="25"
+      color="green"
+      :value="getHP"
+    ></v-progress-linear>
+    {{ this.$store.state.user.HP }}/{{ this.$store.state.user.maxHP }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HPGauge',
+
+  //props: [],
+
+  data: () => ({
+  }),
+
+  methods: {},
+
+  computed: {
+    getHP() {
+      return (this.$store.state.user.HP / this.$store.state.user.maxHP) * 100
+    },
+  },
+
+  created(){
+      
+  },
+}
+</script>
