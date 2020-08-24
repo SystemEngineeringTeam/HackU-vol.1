b283f2eeaf07faf195b7d8d5f3727ba8f269cebc
sonarAIT <sonarmikumiku@gmail.com>
2020-08-24 18:46:20 +0900

    ログの文字色が変わるようにした

--- nuxt/components/Logbox.vue
+++ nuxt/components/Logbox.vue
@@ -3,7 +3,9 @@
     <v-card-title class="justify-center">
       ステータス情報
     </v-card-title>
-    <v-textarea solo readonly v-model="$store.state.game.log"></v-textarea>
+    <v-textarea v-if="getHP <= 25" solo readonly v-model="$store.state.game.log" STYLE="color:#F44336"></v-textarea>
+    <v-textarea v-else-if="getHP <= 50" solo readonly v-model="$store.state.game.log" STYLE="color:#FFEB3B"></v-textarea>
+    <v-textarea v-else solo readonly v-model="$store.state.game.log"></v-textarea>
     <HPGauge />
   </v-card>
 </template>
@@ -16,6 +18,12 @@ export default {
 
   components: {
     HPGauge,
+  },
+
+  computed: {
+    getHP() {
+      return (this.$store.state.game.HP / this.$store.state.game.maxHP) * 100
+    },
   },
 
   data: () => ({}),
