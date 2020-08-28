b382dfc91aa0372f436a9a7b08aba2f61080bca8
kenshin1025 <sukoyakamind1025@gmail.com>
2020-08-28 14:08:59 +0900

    HPがぜろになった時にメッセージを表示するようにした

--- nuxt/components/DieDialog.vue
+++ nuxt/components/DieDialog.vue
@@ -1,26 +1,33 @@
 <template>
   <v-row>
-    <v-btn color="primary" dark @click="$store.commit('game/setHP', 25)">
+    <!-- <v-btn color="primary" dark @click="$store.commit('game/setHP', 25)">
       HP減らす
-    </v-btn>
-    <v-dialog v-model="dialog" max-width="400">
+    </v-btn> -->
+    <v-dialog v-model="dialog" max-width="450">
       <v-card>
-        <v-img
-          src="https://4.bp.blogspot.com/-TalVsZX9oJU/U0pS4v8f_mI/AAAAAAAAe-w/YA6n6klmR6s/s800/kamisama.png"
-        />
+        <v-row justify="center">
+          <v-col cols="6">
+            <v-img
+              src="https://4.bp.blogspot.com/-TalVsZX9oJU/U0pS4v8f_mI/AAAAAAAAe-w/YA6n6klmR6s/s800/kamisama.png"
+            />
+          </v-col>
+        </v-row>
         <v-card-text>
-          君は死んでしまった！
-          あまり無理をするんじゃないぞ…
-          今あるタスクをしっかり終わらせてしばらく休むが良い。
+          <p>{{ this.$store.state.user.name }}、君は死んでしまった！</p>
+          <p>タスクを抱えすぎてしまったようじゃな…</p>
+          <p>時には休息も必要じゃ</p>
+          <p>今あるタスクをしっかり終わらせてしばらく休むが良い。</p>
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="green darken-1" text @click="dialog = false"
-            >Disagree</v-btn
+          <v-btn outlined @click="dialog = false"
+            >はい</v-btn
           >
-          <v-btn color="green darken-1" text @click="dialog = false"
-            >Agree</v-btn
+          <v-spacer></v-spacer>
+          <v-btn outlined @click="dialog = false"
+            >いいえ</v-btn
           >
+          <v-spacer></v-spacer>
         </v-card-actions>
       </v-card>
     </v-dialog>
