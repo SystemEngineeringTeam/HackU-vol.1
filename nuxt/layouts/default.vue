bd64adb2ce8671199dfb6f4cf10c8318ce71ed3a
sonarAIT <sonarmikumiku@gmail.com>
2020-08-28 19:33:38 +0900

    styleの位置を移動した

--- nuxt/layouts/default.vue
+++ nuxt/layouts/default.vue
@@ -44,16 +44,3 @@ export default {
   },
 }
 </script>
-
-<style lang="scss">
-.v-application {
-  font-family: 'PixelMplus';
-}
-
-html,body {
-    width: 100%;
-    margin: 0px;
-    padding: 0px;
-    overflow-x: hidden;
-}
-</style>
